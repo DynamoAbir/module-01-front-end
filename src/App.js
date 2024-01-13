0158a9e4adb6f24f2d9a7817ecca3ea7b7151e76
DynamoAbir <mhabir10@gmail.com>
2024-01-14 02:52:12 +0600

    adding few code

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+      });
+    e.target.reset();
+  };
   useEffect(() => {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+
   return (
     <div className="App">
-      <div>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" id="name" placeholder="Name"></input>
         <input type="email" name="email" id="email" placeholder="Email"></input>
         <button type="submit">submit</button>
-      </div>
+      </form>
 
       <div>
         {users.map((user) => (
